175426fdd3318fe75c76496fbd2691d27b95e749
chetobros <chetobros@gmail.com>
2019-04-04 02:31:01 -0600

    Fixing

--- Assets/Scripts/PipesManager.cs
+++ Assets/Scripts/PipesManager.cs
@@ -52,8 +52,10 @@ public class PipesManager : MonoBehaviour
             }
         }
         ball.SetActive(false);
+        successPanel = PauseMenu.Instance.GetSuccesfullPanel();
         successPanel.SetActive(false);
         dispPipes.text = pipesLimit.ToString();
+       
     }
     void Update()
     {
